09b8506801d2b5b9afb9f825e1ed562cdcf71004
lennon1230 <lennongama751@gmail.com>
2024-04-30 22:12:07 -0300

    Update Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { TermsComponent } from './pages/terms/terms.component';
 import { PrivacyComponent } from './pages/privacy/privacy.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 export const routes: Routes = [
     {'path': '', 'title': 'Home', component:IndexComponent},
@@ -15,5 +16,6 @@ export const routes: Routes = [
     {"path": 'contact', 'title': 'Contact', component:ContactComponent},
     {'path': 'terms-of-use', 'title': 'Terms of Use', component:TermsComponent},
     {'path': 'privacy-policy', 'title': 'Privacy Policy', component:PrivacyComponent},
+    {'path': 'blog', 'title': 'Blog', component:BlogComponent},
     {'path': '**', 'title': 'Page Not Found', component:PageNotFoundComponent},
 ];
--- src/app/pages/index/main/main.component.ts
+++ src/app/pages/index/main/main.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { BigCardComponent } from './big-card/big-card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [BigCardComponent],
+  imports: [BigCardComponent, NgFor],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+
+  paginator :number = 5;
+  classBtnHidden :boolean = false;
+
+  pagination(){
+    if(this.paginator < 10)
+      this.paginator += 5;
+    else{
+      this.paginator += 5;
+      this.classBtnHidden = true;
+    }
+  }
 
 }
